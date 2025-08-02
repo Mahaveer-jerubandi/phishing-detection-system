1c4c9203774e131680c671087b3f6e7cbed6be04
Mahaveer-jerubandi <jerubandimahaveer@gmail.com>
2025-08-02 15:17:33 +0100

    Delete real-time-phishing-detection directory

--- real-time-phishing-detection/utils/virus_total.py
+++
@@ -1,9 +0,0 @@
-import requests
-API_KEY = "YOUR_VIRUSTOTAL_API_KEY"
-
-def check_url_virustotal(url):
-    try:
-        resp = requests.post('https://www.virustotal.com/vtapi/v2/url/scan', data={'apikey': API_KEY, 'url': url})
-        return {'success': True, 'message': 'Submitted to VirusTotal.'}
-    except:
-        return {'success': False, 'message': 'API error.'}
